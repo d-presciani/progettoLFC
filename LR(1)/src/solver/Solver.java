284b15135d9954bb33dc361bfccc3c71ff8ce4e5
Unknown <d.presciani@gmail.com>
2019-03-11 22:31:46 +0100

    Caricamento workspace Eclipse
    Classe stato non funzionante

---
+++ LR(1)/src/solver/Solver.java
@@ -0,0 +1,41 @@
+package solver;
+
+import java.util.List;
+import java.util.LinkedList;
+
+public class Solver {
+	
+	
+	public void solve(LinkedList<NonTerminale> listaNT, LinkedList<RegolaDiProduzione> listaReg) {
+		// Aggiungo ad ogni non terminale le sue regole
+		for(RegolaDiProduzione reg : listaReg) {
+			for(NonTerminale nt : listaNT) {
+				if(reg.parteSX.lettera.equals(nt.lettera)) {
+					nt.addRegola(reg);
+					break;
+				}
+			}	
+		}
+		
+		//List<Stato> listaStati = new LinkedList<Stato>();
+
+		//Creo il primo stato
+		Stato mom = new Stato();
+		mom.agiungiCore(listaReg.get(0));
+		
+		System.out.println(mom.toString());
+		
+		//Calcolo annullabilità regole
+		for(RegolaDiProduzione reg : listaReg) {
+			reg.calcolaAnnullabilita();
+		}
+		
+		//Calcolo annullabilità caratteri
+		for (NonTerminale nt: listaNT) {
+			nt.calcolaAnnullabile();
+		}
+		
+		
+		
+	}
+}
---
+++ LR(1)/src/solver/Stato.java
@@ -0,0 +1,111 @@
+package solver;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Stato {
+	static int counter = 0;
+	List<RegolaDiProduzione> regoleCore;
+	List<RegolaDiProduzione> regoleCompletamenti;
+	int numeroStato;
+	
+	
+	public Stato() {
+		numeroStato = counter;
+		counter++;
+		regoleCore = new LinkedList<RegolaDiProduzione>();
+		regoleCompletamenti = new LinkedList<RegolaDiProduzione>();
+	}
+	
+	// Da sistemare
+	public void agiungiCore(RegolaDiProduzione rdp) {
+		regoleCore.add(new RegolaDiProduzione(rdp));
+		if(rdp.parteDX!=null) {
+			for(RegolaDiProduzione regComp : rdp.parteDX.get(rdp.indice).getRegole()) {
+				RegolaDiProduzione tmp = new RegolaDiProduzione(regComp);
+				if(rdp.indice<rdp.parteDX.size()) {
+					int i = 0;
+					while(rdp.parteDX.get(rdp.indice+i).isAnnullabile()) {
+						tmp.seguiti.addAll(rdp.parteDX.get(rdp.indice+i+1).calcolaInizi());
+					}
+					if(rdp.annullabile) {
+						tmp.seguiti.addAll(rdp.seguiti);
+					}
+				} else {
+					tmp.seguiti.addAll(rdp.seguiti);
+				}
+				regoleCompletamenti.add(tmp);
+			}
+			for(RegolaDiProduzione reg : regoleCompletamenti) {
+				for(RegolaDiProduzione nuoveRegole: reg.parteDX.get(reg.indice).getRegole()) {
+					boolean trovato = false;
+					for(RegolaDiProduzione regCk : regoleCompletamenti) {
+						if(nuoveRegole.parteSX.toString().equals(regCk.parteSX.toString()) && nuoveRegole.parteDX.toString().equals(regCk.parteDX.toString())) {
+							trovato = true;
+							break;
+						}
+					}
+					if(!trovato) {
+						this.aggiungiCompletameno(nuoveRegole);
+					}
+				}
+			}
+		}
+	}
+	
+	// Da sistemare
+	public void aggiungiCompletameno(RegolaDiProduzione rdp) {
+		regoleCompletamenti.add(new RegolaDiProduzione(rdp));
+		if(rdp.parteDX!=null && rdp.parteDX.size()!=0) {
+			if(rdp.parteDX.get(rdp.indice) instanceof NonTerminale) {
+				for(RegolaDiProduzione regComp : rdp.parteDX.get(rdp.indice).getRegole()) {
+					RegolaDiProduzione tmp = new RegolaDiProduzione(regComp);
+					if(rdp.indice<rdp.parteDX.size()) {
+						int i = 0;
+						while(rdp.parteDX.get(rdp.indice+i).isAnnullabile()) {
+							tmp.seguiti.addAll(rdp.parteDX.get(rdp.indice+i+1).calcolaInizi());
+						}
+						if(rdp.annullabile) {
+							tmp.seguiti.addAll(rdp.seguiti);
+						}
+					} else {
+						tmp.seguiti.addAll(rdp.seguiti);
+					}
+					regoleCompletamenti.add(tmp);
+				}
+				for(RegolaDiProduzione reg : regoleCompletamenti) {
+					boolean trovato = false;
+					for(RegolaDiProduzione regCk : regoleCompletamenti) {
+						if(reg.parteSX.toString().equals(regCk.parteSX.toString()) && reg.parteDX.toString().equals(regCk.parteDX.toString())) {
+							trovato = true;
+							break;
+						}
+					}
+					if(!trovato) {
+						this.aggiungiCompletameno(reg);
+					}
+				}
+			}
+		}
+	}
+	
+	public void espandiStato() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		String temp = "";
+		temp += "Stato: S" + numeroStato + "\nRegole Core:\n";
+		for(RegolaDiProduzione reg : regoleCore) {
+			temp += reg.toString() + " {" + reg.seguiti + "} \n";
+		}
+		temp += "\n-----\nRegole completamento:\n" ;
+		for(RegolaDiProduzione reg : regoleCompletamenti) {
+			temp += reg.toString() + " {" + reg.seguiti + "} \n";
+		}
+		return temp;
+	}
+	
+		
+}
