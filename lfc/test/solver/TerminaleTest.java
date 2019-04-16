05bd7c9cf40268785a0754a20d0fe953475c38e7
l-filice <l.filice@studenti.unibg.it>
2019-04-16 12:09:37 +0200

    Ultimazione test classe Terminale

---
+++ lfc/test/solver/TerminaleTest.java
@@ -0,0 +1,57 @@
+package solver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class TerminaleTest {
+
+	@Test
+	void costruttore() {
+		Terminale ta = new Terminale("a");
+		assertEquals("a", ta.lettera);
+	}
+	
+	@Test
+	void calcoloInizi() {
+		Terminale ta = new Terminale("a");
+		LinkedList<String> check = new LinkedList<String>();
+		check.add("a");
+		List<String> inizi = ta.calcolaInizi(new LinkedList<RegolaDiProduzione>());
+		assertEquals(check, inizi);
+	}
+	
+	@Test
+	void testToString() {
+		Terminale ta = new Terminale("a");
+		assertEquals("a", ta.toString());
+	}
+	
+	@Test
+	void annullabile() {
+		Terminale ta = new Terminale("a");
+		assertFalse(ta.isAnnullabile());
+	}
+	
+	@Test
+	void lettera() {
+		Terminale ta = new Terminale("a");
+		assertEquals("a", ta.getLettera());
+	}
+	
+	@Test
+	void listaRegole() {
+		Terminale ta = new Terminale("a");
+		assertNull(ta.getRegole());
+	}
+	
+	
+	@Test
+	void terminale() {
+		Terminale ta = new Terminale("a");
+		assertTrue(ta.isTerminale());
+	}
+}
